055d21a193007b2ae3cca838fb852522603341cc
Ben Koo <koo0905@gmail.com>
2025-08-20 17:27:41 +0800

    Fixed some warning of unused icons

--- src/components/Dashboard/TopBar.tsx
+++ src/components/Dashboard/TopBar.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { Button } from "@/components/ui/button";
-import { LogIn, User, LogOut } from 'lucide-react';
+import { LogIn, LogOut } from 'lucide-react';
 import {
   DropdownMenu,
   DropdownMenuContent,
